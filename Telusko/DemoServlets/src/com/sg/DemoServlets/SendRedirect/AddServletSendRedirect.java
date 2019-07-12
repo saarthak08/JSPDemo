e32aa366c42c003f309f22b93b583d2239ade13e
Saarthak Gupta <saarthakgupta08@gmail.com>
2019-07-12 16:12:13 +0530

    SendRedirect via Session & Cookies

--- Telusko/DemoServlets/src/com/sg/DemoServlets/SendRedirect/AddServletSendRedirect.java
+++ Telusko/DemoServlets/src/com/sg/DemoServlets/SendRedirect/AddServletSendRedirect.java
@@ -4,9 +4,11 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class AddServletSendRedirect extends HttpServlet{
 	public void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
@@ -15,10 +17,21 @@ public class AddServletSendRedirect extends HttpServlet{
 		
 		int k=i+j;
 		
-		httpServletRequest.setAttribute("k", k);		
+		httpServletRequest.setAttribute("k", k);
 		
-		httpServletResponse.sendRedirect("sqsr?k="+k); //URL Rewriting
+		/* Setting attribute using Session Management
+		HttpSession session=httpServletRequest.getSession();
+		session.setAttribute("k", k);
+		*/
 		
+		
+		//Using Cookies
+		Cookie cookie=new Cookie("k", k+"");
+		httpServletResponse.addCookie(cookie);
+		
+		//httpServletResponse.sendRedirect("sqsr?k="+k); //Using URL Rewriting
+		
+		httpServletResponse.sendRedirect("sqsr"); 
 	}
 
 
--- Telusko/DemoServlets/src/com/sg/DemoServlets/SendRedirect/SquareServletSendRedirect.java
+++ Telusko/DemoServlets/src/com/sg/DemoServlets/SendRedirect/SquareServletSendRedirect.java
@@ -2,19 +2,44 @@ package com.sg.DemoServlets.SendRedirect;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.CookieStore;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class SquareServletSendRedirect extends HttpServlet {
+	int k;
+	
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		PrintWriter writer=response.getWriter();
 		writer.println("Hello to square!\n\n");
-		int k=Integer.parseInt(request.getParameter("k"));
+		
+		
+		/* Retrieving Value using Session Management
+		HttpSession session=request.getSession();
+		int k=(int) session.getAttribute("k");
+		*/
+
+		//Using Cookies
+		 Cookie[] cookies=request.getCookies();
+		 for(Cookie c:cookies) {
+			 if(c.getName().equals("k")) {
+				 k=Integer.parseInt(c.getValue());
+			 }
+		 }
+		
+		
+		//int k=Integer.parseInt(request.getParameter("k"));
+		
+		
 		writer.println("Resultant Sum: "+k);
 		k=k*k;
 		writer.println("Resultant square: "+k);
+		
+		//session.removeAttribute("k");
 		
 	}
 
