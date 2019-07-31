3e0db1de9e87c8aa0e495ff08ba35a8b80041b4c
Saarthak <saarthakgupta08@gmail.com>
2019-07-31 22:41:33 +0530

    Added Java Annotations for SendRedirect Servlets

--- Telusko/DemoServlets/src/com/sg/DemoServlets/SendRedirect/AddServletSendRedirect.java
+++ Telusko/DemoServlets/src/com/sg/DemoServlets/SendRedirect/AddServletSendRedirect.java
@@ -4,12 +4,15 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+
+@WebServlet("/addsr")
 public class AddServletSendRedirect extends HttpServlet{
 	public void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
 		int i=Integer.parseInt(httpServletRequest.getParameter("num1"));
--- Telusko/DemoServlets/src/com/sg/DemoServlets/SendRedirect/SquareServletSendRedirect.java
+++ Telusko/DemoServlets/src/com/sg/DemoServlets/SendRedirect/SquareServletSendRedirect.java
@@ -5,12 +5,15 @@ import java.io.PrintWriter;
 import java.net.CookieStore;
 
 import javax.servlet.ServletContext;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+
+@WebServlet("/sqsr")
 public class SquareServletSendRedirect extends HttpServlet {
 	int k;
 	
