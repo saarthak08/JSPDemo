38fe39f4dda047fcdb0e958b254047239f9dd8a4
Saarthak <saarthakgupta08@gmail.com>
2019-08-23 02:30:21 +0530

    Implemented Reading form data in Servlets

---
+++ ChadDarby/Demo JSP/src/com/sg/jsp/Servlets/ReadingFormData.java
@@ -0,0 +1,23 @@
+package com.sg.jsp.Servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+@WebServlet("/Servlets/readingFormData")
+public class ReadingFormData extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String firstName=(String)req.getParameter("firstName");
+        String lastName=(String)req.getParameter("lastName");
+        PrintWriter out=resp.getWriter();
+        out.println("First Name: "+ firstName);
+        out.println("Last Name: "+lastName);
+    }
+}
